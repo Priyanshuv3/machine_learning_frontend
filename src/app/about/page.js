fc16763ed6128cc7fc9b82363049414ddd9c92d9
priyanshu verma <priyanshuv3@gmail.com>
2025-01-19 13:18:27 +0530

    special file added

---
+++ src/app/about/page.js
@@ -0,0 +1,14 @@
+export default function AboutPage() {
+    return (
+      <div style={{ textAlign: 'center', padding: '50px' }}>
+        <h1>About Us</h1>
+        <p>
+          Welcome to the About page! Here you can learn more about our mission, values, and team.
+        </p>
+        <a href="/" style={{ color: 'blue', textDecoration: 'underline' }}>
+          Go back to the homepage
+        </a>
+      </div>
+    );
+  }
+  
